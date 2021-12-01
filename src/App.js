cd01e1d4bddec6548984b5a14cd1541a5e6fc518
Kanagaraj <kanagarajwhb@gmail.com>
2021-12-01 22:38:11 +0530

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Navbar from "./components/Navbar";
 
 function App() {
   const [kural, setKural] = useState([]);
+  const [kuralNumber, setKuralNumber] = useState(1);
 
   useEffect(() => {
     axios
-      .get(`https://api-thirukkural.vercel.app/api?num=1`)
+      .get(`https://api-thirukkural.vercel.app/api?num=${kuralNumber}`)
       .then((response) => {
         console.log(response.data);
         setKural(response.data);
       });
-  }, []);
+  }, [kuralNumber]);
 
- 
+  const nextKural = () => {
+    setKuralNumber(kuralNumber + 1);
+  };
 
   return (
     <div>
@@ -23,7 +26,7 @@ function App() {
       <h3>{kural.chap_tam}</h3>
       <h3>{kural.line1}</h3>
       <h3>{kural.line2}</h3>
-      
+      <button onClick={nextKural}>Next Kural</button>
     </div>
   );
 }
